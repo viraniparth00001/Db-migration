6247576b4bd390ee6dcde0edcde1527836159c5e
Parth Virani <parthv.streamspace@gmail.com>
2022-06-27 15:29:27 +0530

    changes

---
+++ 1656323795.js
@@ -0,0 +1,36 @@
+module.exports = {
+	async up(db, redis) {
+		try {
+			await db
+				.collection("albums")
+				.updateOne(
+					{ artist: "The Beatles" },
+					{ $set: { blacklisted: true } },
+				);
+			await db
+				.collection("albums")
+				.updateOne({ artist: "The Doors" }, { $set: { stars: 5 } });
+		} finally {
+			//await session.endSession();
+		}
+	},
+
+	async down(db, client) {
+		//const session = client.startSession();
+		try {
+			//await session.withTransaction(async () => {
+			await db
+				.collection("albums")
+				.updateOne(
+					{ artist: "The Beatles" },
+					{ $set: { blacklisted: false } },
+				);
+			await db
+				.collection("albums")
+				.updateOne({ artist: "The Doors" }, { $set: { stars: 0 } });
+			//});
+		} finally {
+			//await session.endSession();
+		}
+	},
+};
