0d6788f2a2b3059ef75ef0821aa8a301f7c45efa
Parth Virani <parthv.streamspace@gmail.com>
2022-07-26 17:10:44 +0530

    changes releted

--- 1656323799.js
+++ 1656323799.js
@@ -1,39 +1,39 @@
 module.exports = {
 	async up(db, redis) {
-		try {
-			await db
-				.collection("1albums")
-				.updateOne(
-					{ artist: "The Beatles 222222" },
-					{ $set: { blacklisted: false } },
-				);
-			await db
-				.collection("1albums")
-				.updateOne(
-					{ artist: "The Doors2222222" },
-					{ $set: { stars: 6 } },
-				);
-		} finally {
-			//await session.endSession();
-		}
+		// try {
+		// 	await db
+		// 		.collection("1albums")
+		// 		.updateOne(
+		// 			{ artist: "The Beatles 222222" },
+		// 			{ $set: { blacklisted: false } },
+		// 		);
+		// 	await db
+		// 		.collection("1albums")
+		// 		.updateOne(
+		// 			{ artist: "The Doors2222222" },
+		// 			{ $set: { stars: 6 } },
+		// 		);
+		// } finally {
+		// 	//await session.endSession();
+		// }
 	},
 
 	async down(db, client) {
 		//const session = client.startSession();
-		try {
-			//await session.withTransaction(async () => {
-			await db
-				.collection("albums")
-				.updateOne(
-					{ artist: "The Beatles" },
-					{ $set: { blacklisted: false } },
-				);
-			await db
-				.collection("albums")
-				.updateOne({ artist: "The Doors" }, { $set: { stars: 0 } });
-			//});
-		} finally {
-			//await session.endSession();
-		}
+		// try {
+		// 	//await session.withTransaction(async () => {
+		// 	await db
+		// 		.collection("albums")
+		// 		.updateOne(
+		// 			{ artist: "The Beatles" },
+		// 			{ $set: { blacklisted: false } },
+		// 		);
+		// 	await db
+		// 		.collection("albums")
+		// 		.updateOne({ artist: "The Doors" }, { $set: { stars: 0 } });
+		// 	//});
+		// } finally {
+		// 	//await session.endSession();
+		// }
 	},
 };
